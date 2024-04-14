7f7cb40191769fc9faf9f84b5c1814010fa8e733
guisoLazaro <gsolazaro@gmail.com>
2024-04-14 20:31:19 -0300

    Area de comentarios pagina açoes

---
+++ Ações/acoesScript.js
@@ -0,0 +1,20 @@
+function adicionarComentario() {
+    var areaComentarios = document.getElementById('listaComentarios');
+    var textoComentario = document.getElementById('textoComentario').value;
+    var textoNome = document.getElementById('nomeUsuario').value
+    var divComentario = document.createElement('div');
+    var divNome = document.createElement('div')
+    divNome.classList.add('nomeComentario')
+    divNome.textContent = textoNome + ':'
+    divComentario.classList.add('comentario');
+    divComentario.textContent = textoComentario;
+    if(divComentario.textContent !== ''){
+        areaComentarios.appendChild(divNome);
+        areaComentarios.appendChild(divComentario);
+    }else{
+        alert("Por favor, insira um comentário antes de fazer o envio!")
+    }
+    document.getElementById('nomeUsuario').value = ''
+    document.getElementById('textoComentario').value = ''; // Limpa o campo de texto
+  }
+  
