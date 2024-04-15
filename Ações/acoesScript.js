ab607614f6fbc0339fe7daef1dd577f75acf7f89
guisoLazaro <gsolazaro@gmail.com>
2024-04-15 20:57:22 -0300

    Ajustes na area de comentários

--- Ações/acoesScript.js
+++ Ações/acoesScript.js
@@ -6,11 +6,11 @@ function adicionarComentario() {
     var divNome = document.createElement('div')
     divNome.classList.add('nomeComentario')
     divNome.textContent = textoNome + ':'
-    divComentario.classList.add('comentario');
-    divComentario.textContent = textoComentario;
+    divComentario.classList.add('comentario')
+    divComentario.textContent = textoComentario
     if(divComentario.textContent !== ''){
-        areaComentarios.appendChild(divNome);
-        areaComentarios.appendChild(divComentario);
+        areaComentarios.appendChild(divNome)
+        areaComentarios.appendChild(divComentario)
     }else{
         alert("Por favor, insira um comentário antes de fazer o envio!")
     }
